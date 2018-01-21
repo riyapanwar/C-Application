ccf4de6e911961fc783b8817d7f60a5f4a561a40
Riya Panwar <rpanwar@ex2india.com>
2018-01-21 14:05:16 +0530

    Rock Paper Scissor Game

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace RPS_Game
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {   int usermove,compmove,totround=0,win=0,lost=0,draw=0;
+            string user,chance;
+            Console.WriteLine("Welcome to Rock Paper Scissor Game!!!!");
+            do
+            {
+            Console.WriteLine("Select your move:- \n 1.Rock \n 2.Paper \n 3.Scissor");
+            user=Console.ReadLine();
+            bool checkinput = int.TryParse(user, out usermove);
+            if(checkinput)
+            { usermove=Convert.ToInt32(user);
+            Random computer = new Random();
+            compmove=computer.Next(1,4);
+            totround++;
+            switch(usermove)
+            {
+                case 1 : 
+                switch(compmove)
+                { case 1: Console.WriteLine("Computer chose rock. \nIts a tie!!!"); draw++;
+                  break;
+                  case 2 : Console.WriteLine("Computer chose paper. \nYou Lost!!!"); lost++;
+                  break;
+                  case 3 : Console.WriteLine("Computer chose scissor. \nYou Won!!!"); win++;
+                  break;
+                }
+                break;
+                case 2 :
+                switch(compmove)
+                { case 1: Console.WriteLine("Computer chose rock. \nYou Won!!!"); win++;
+                  break;
+                  case 2 : Console.WriteLine("Computer chose paper. \nIts a tie!!!"); draw++;
+                  break;
+                  case 3 : Console.WriteLine("Computer chose scissor. \nYou Lost!!!"); lost++;
+                  break;
+                }
+                break;
+                case 3 :
+                switch(compmove)
+                { case 1: Console.WriteLine("Computer chose rock. \nYou Lost!!!"); lost++;
+                  break;
+                  case 2 : Console.WriteLine("Computer chose paper. \nYou Won!!!"); win++;
+                  break;
+                  case 3 : Console.WriteLine("Computer chose scissor. \nIts a tie!!!"); draw++;
+                  break;
+                }
+                break;
+                default : Console.WriteLine("Invalid Input"); totround--;
+                break;
+            }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            do
+            { Console.WriteLine("Do you want to try again??? (Y/N)");
+            chance=Console.ReadLine();
+            if(!(chance.Equals("Y") || chance.Equals("N") ))
+            { Console.WriteLine("Invalid Input"); }
+            }while(!(chance.Equals("Y") || chance.Equals("N") ));
+            }while(chance.Equals("Y"));
+            Console.WriteLine("TOTAL ROUNDS \t WON \t DRAW \t LOST");
+            Console.Write("{0} \t\t ",totround);
+            Console.Write("{0} \t ",win);
+            Console.Write("{0} \t ",draw);
+            Console.Write("{0}\n",lost);
+            if(chance.Equals("N"))
+            { Console.WriteLine("Come back soon..."); }
+            Console.ReadKey();
+        }
+    }
+}
+
